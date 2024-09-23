52f982e29d07639d1623157251e7b2a66688cab2
Cyanix-0721 <34270450+Cyanix-0721@users.noreply.github.com>
2024-09-23 17:22:34 +0800

    chore: 更新 giscus 配置，启用评论功能并获取前置数据

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -144,39 +144,39 @@ const ExtendedTheme: Theme = {
     })
   },
   setup() {
-    // Get frontmatter and route
-    // const { frontmatter } = toRefs(useData())
-    // const route = useRoute()
+    Get frontmatter and route
+    const { frontmatter } = toRefs(useData())
+    const route = useRoute()
 
-    // Obtain configuration from: https://giscus.app/
-    // giscusTalk({
-    //   repo: 'wryyyy4444/Blog_nolebase',
-    //   repoId: 'R_kgDOMaBljQ',
-    //   category: 'Announcements', // default: `General`
-    //   categoryId: 'DIC_kwDOMaBljc4ChG01',
-    //   mapping: 'title', // default: `pathname`
-    //   inputPosition: 'top', // default: `top`
-    //   lang: 'zh-CN', // default: `zh-CN`
-    //   // i18n setting (Note: This configuration will override the default language set by lang)
-    //   // Configured as an object with key-value pairs inside:
-    //   // [your i18n configuration name]: [corresponds to the language pack name in Giscus]
-    //   locales: {
-    //     'zh-Hans': 'zh-CN',
-    //     'en-US': 'en',
-    //   },
-    //   homePageShowComment: false, // Whether to display the comment area on the homepage, the default is false
-    //   lightTheme: 'light', // default: `light`
-    //   darkTheme: 'transparent_dark', // default: `transparent_dark`
-    //   // ...
-    // }, {
-    //   frontmatter,
-    //   route,
-    // },
-    //   // Whether to activate the comment area on all pages.
-    //   // The default is true, which means enabled, this parameter can be ignored;
-    //   // If it is false, it means it is not enabled.
-    //   // You can use `comment: true` preface to enable it separately on the page.
-    //   true)
+    Obtain configuration from: https://giscus.app/
+    giscusTalk({
+      repo: 'Cyanix-0721/blog-nolebase',
+      repoId: 'R_kgDOMhUn6Q',
+      category: 'Announcements', // default: `General`
+      categoryId: 'DIC_kwDOMhUn6c4CitCd',
+      mapping: 'pathname', // default: `pathname`
+      inputPosition: 'top', // default: `top`
+      lang: 'zh-CN', // default: `zh-CN`
+      // i18n setting (Note: This configuration will override the default language set by lang)
+      // Configured as an object with key-value pairs inside:
+      // [your i18n configuration name]: [corresponds to the language pack name in Giscus]
+      locales: {
+        'zh-Hans': 'zh-CN',
+        'en-US': 'en',
+      },
+      homePageShowComment: false, // Whether to display the comment area on the homepage, the default is false
+      lightTheme: 'light', // default: `light`
+      darkTheme: 'transparent_dark', // default: `transparent_dark`
+      // ...
+    }, {
+      frontmatter,
+      route,
+    },
+      // Whether to activate the comment area on all pages.
+      // The default is true, which means enabled, this parameter can be ignored;
+      // If it is false, it means it is not enabled.
+      // You can use `comment: true` preface to enable it separately on the page.
+      true)
   },
 }
 
